c10332cedbed721ca07deafad521f40e80411ccc
murodbeksindarov <murodbeksindarov@gmail.com>
2024-05-14 16:41:53 +0500

    config

--- data/config.py
+++ data/config.py
@@ -1,16 +1,16 @@
-# from environs import Env
+from environs import Env
 # import os
 # # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
+env = Env()
+env.read_env()
 
 # # .env fayl ichidan quyidagilarni o'qiymiz
 
 
 import os
-BOT_TOKEN= str(os.environ.get("BOT_TOKEN"))
-IP= str(os.environ.get("ip"))
-ADMINS=list(os.environ.get('ADMINS'))
+BOT_TOKEN= env.str("BOT_TOKEN")
+IP= env.str('ip')
+ADMINS=env.list('ADMINS')
 
 kanallar = []
 video_kanal = ['@IELTS_CEFR_lesson']
