0e50f7c71c6446b4a48c29cf2e1640db4d9c169c
Diyorbek Bahodirov <bahodirovdiyorbek2007@gmail.com>
2024-05-14 16:31:05 +0500

    dd

--- data/config.py
+++ data/config.py
@@ -1,16 +1,19 @@
-# from environs import Env
-# import os
-# # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
+from environs import Env
+import os
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
 
 # # .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")
+IP = env.str('ip')
+ADMINS = env.list('ADMINS')
+#
+# import os
+# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+# IP = str(os.environ.get("ip"))
+# ADMINS = list(os.environ.get('ADMINS'))
 
-
-import os
-BOT_TOKEN= str(os.environ.get("BOT_TOKEN"))
-IP= str(os.environ.get("ip"))
-ADMINS=list(os.environ.get('ADMINS'))
 
 kanallar = []
 video_kanal = ['@IELTS_CEFR_lesson']
