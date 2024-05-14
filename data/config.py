86d87581199e3ecb0f7a92dde6841ca79007c2cd
murodbeksindarov <67220507+murodbeksindarov@users.noreply.github.com>
2024-05-14 19:31:37 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,15 +1,13 @@
 from environs import Env
-# # environs kutubxonasidan foydalanish
 
-import os
 # environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
 
-# # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")
-IP = env.str('ip')
-ADMINS = env.list('ADMINS')
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("ip")
 #
 # import os
 # BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
